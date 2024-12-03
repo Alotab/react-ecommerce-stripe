26f272f8c6b84621866a0e8b3843de6f6f680712
Albert Osei Frimpong <alotab@yahoo.com>
2024-12-02 18:38:07 -0800

    setup project

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+
+import Container from './ui/Container'
+import Footer from './ui/Footer'
+import Header from './ui/Header'
+
+function App() {
+
+
+  return (
+    <main>
+      <Header />
+      <Container>
+        <p>Ecommmerce</p>
+      </Container>
+      <Footer />
+    </main>
+  )
+}
+
+export default App
